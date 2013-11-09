11d2560f049883188cab82fa6fa924b041963732
Harold Rodriguez <harold@planetharold.com>
2013-11-09 14:51:23 -0500

    make reloadr recursively check directories

--- reloadr.php
+++ reloadr.php
@@ -1,14 +1,15 @@
 <?php
-
-$filename_lists = array_map('glob', explode(',', $_SERVER['QUERY_STRING']));
-
+$filename_lists = array_map('glob_recursive', explode(',', $_SERVER['QUERY_STRING']));
 $files = array();
-
-foreach( $filename_lists as $filename_list )
-	$files = array_merge($files, $filename_list);
-	
-foreach ( $files as &$file )
-	$file = filemtime($file);
-	
+foreach( $filename_lists as $filename_list ) $files = array_merge($files, $filename_list);
+foreach ( $files as &$file ) $file = filemtime($file);
 header('Last-Modified: '. date('r', @max($files)));
+
+function glob_recursive($pattern, $flags = 0) {
+    $files = glob($pattern, $flags);
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+    }
+    return $files;
+}
 ?>
